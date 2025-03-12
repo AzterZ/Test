0b4c0a4f74ae41e61b619532dde43426d5c06739
DanielWasilewski08 <danielwasilewski08@gmail.com>
2025-03-12 18:22:10 +0100

    Update test.py
    added function "ile_dzielnikow"

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 print("Hello World!")
 n = int(input())
 print(n)
+
+var1 = 25
+def ile_dzielnikow(a):
+  sum = 0
+  for i in range(1,a+1):
+    if a%i == 0:
+      sum += 1
+  print(sum)
+ile_dzielnikow(var1)
